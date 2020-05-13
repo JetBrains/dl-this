f2e3bdae1cad822977d528653c6201ea825bc087
IGA <aujot99@gmail.com>
2020-05-13 18:09:17 +0300

    Improved download by link

--- backend/src/main/kotlin/Download.kt
+++ backend/src/main/kotlin/Download.kt
@@ -1,20 +1,21 @@
-import java.io.InputStream
+
 import java.net.URL
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 
-
-fun download(urlValue: String, filepath: String = "D:/Tests/file_name.txt" ){
-    val url = URL(urlValue)
-    val inputStream = url.openStream()
-    Files.copy(inputStream, Paths.get(filepath), StandardCopyOption.REPLACE_EXISTING)
-
+fun getFileNameFromUrl(url: URL): String {
+    val urlPath = url.path
+    return urlPath.substring(urlPath.lastIndexOf('/') + 1)
 }
 
+fun download(urlValue: String, filePath: String = "D:/Tests", file_Name: String = "defaultSettings" ){
+    val url = URL(urlValue)
+    var fileName = file_Name
+    if (fileName == "defaultSettings"){
+        fileName = getFileNameFromUrl(url)
+    }
+    val inputStream = url.openStream()
+    Files.copy(inputStream, Paths.get("$filePath/$fileName"), StandardCopyOption.REPLACE_EXISTING)
 
-
-
-
-/*InputStream inputStream = new URL("http://example.com/my-file-path.txt").openStream()
-Files.copy(inputStream, Paths.get("/Users/username/Documents/file_name.txt"), StandardCopyOption.REPLACE_EXISTING)*/
+}
--- backend/src/test/kotlin/DownloadTest.kt
+++ backend/src/test/kotlin/DownloadTest.kt
@@ -4,7 +4,14 @@ import io.kotest.matchers.shouldBe
 
 class DownloadTest : FreeSpec({
 
-    "downloadLink"  {
-        download("https://vk.com/doc132800647_548972002?hash=eccee483abefba8d39&dl=174d2cb1ea16038c30", "D:/Tests/1.txt")
+    "downloadLink" - {
+       "Download with default settings" {
+            download("https://vk.com/doc132800647_548972002?hash=eccee483abefba8d39&dl=174d2cb1ea16038c30")
+        }
+        "Download with parameters" {
+            download("https://bipbap.ru/wp-content/uploads/2017/10/0_8eb56_842bba74_XL-640x400.jpg", file_Name = "image.jpg" )
+        }
+
     }
+
 })
