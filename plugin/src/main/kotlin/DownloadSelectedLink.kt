2eaf57fef67884ca709815d36ded535d51495b4c
SerVB <servbul@yandex.ru>
2020-05-14 00:59:20 +0300

    Implement Download Selected Link action

---
+++ plugin/src/main/kotlin/DownloadSelectedLink.kt
@@ -0,0 +1,32 @@
+import com.intellij.ide.impl.ProjectUtil
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.Project
+
+class DownloadSelectedLink : AnAction() {
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val currentProject = event.project ?: return
+        val destinationDir = getDestinationDir(currentProject)
+
+        val selectedText = event.getData(CommonDataKeys.CARET)?.selectedText
+
+        if (selectedText != null) {
+            DownloadLauncher.runDownloadsInBackground(currentProject, listOf(selectedText), destinationDir)
+        }
+    }
+
+    override fun update(event: AnActionEvent) {
+        val data = event.getData(CommonDataKeys.CARET)
+
+        event.presentation.isEnabledAndVisible = data?.hasSelection() ?: false
+    }
+
+    companion object {
+
+        private fun getDestinationDir(project: Project?): String {
+            return ProjectUtil.getBaseDir()  // todo: use project to determine dir
+        }
+    }
+}
