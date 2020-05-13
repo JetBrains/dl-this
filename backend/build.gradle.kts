05dd6c68d1c3e0fd76d88f8667f4491cadde4426
IGA <aujot99@gmail.com>
2020-05-13 20:01:17 +0300

    Return build.gradle

---
+++ backend/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm")
+}
+
+val kotestVersion: String by project
+val kotlinVersion: String by project
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+
+
+    implementation("com.turn:ttorrent-core:1.5")
+
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.0.5")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.0.5")
+    testImplementation("io.kotest:kotest-property-jvm:4.0.5")
+}
