8a9e116fb0047df247bf27c9d579722c572ab16d
IGA <aujot99@gmail.com>
2020-05-13 18:22:54 +0300

    Return Exception

--- backend/src/main/kotlin/Download.kt
+++ backend/src/main/kotlin/Download.kt
@@ -4,18 +4,23 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 
+class BadUrlException : Throwable()
+
 fun getFileNameFromUrl(url: URL): String {
     val urlPath = url.path
     return urlPath.substring(urlPath.lastIndexOf('/') + 1)
 }
 
-fun download(urlValue: String, filePath: String = "D:/Tests", file_Name: String = "defaultSettings" ){
+fun downloadByURL(urlValue: String, filePath: String = "", file_Name: String = "defaultSettings" ){
+    if ("http" !in urlValue) {
+        throw BadUrlException()
+    }
     val url = URL(urlValue)
     var fileName = file_Name
     if (fileName == "defaultSettings"){
         fileName = getFileNameFromUrl(url)
     }
     val inputStream = url.openStream()
-    Files.copy(inputStream, Paths.get("$filePath/$fileName"), StandardCopyOption.REPLACE_EXISTING)
+    Files.copy(inputStream, Paths.get("$filePath$fileName"), StandardCopyOption.REPLACE_EXISTING)
 
 }
